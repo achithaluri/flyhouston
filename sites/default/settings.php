cb1363a6345952db4dbbb9557ee34bd21708554e
chandanpaul <er.cpaul@gmail.com>
2020-02-14 20:25:25 +0530

    add db file

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -756,7 +756,7 @@ $settings['entity_update_backup'] = TRUE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'houston',
+  'database' => 'fly2houston',
   'username' => 'root',
   'password' => '',
   'prefix' => '',
