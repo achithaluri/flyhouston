d45808d7b8af6b285669bc54445f83d181e6b263
chandanpaul <er.cpaul@gmail.com>
2020-02-16 23:39:07 +0530

    update code for menu

---
+++ sites/default/files/php/twig/5e46b7fb9f785_paragraph--hero-area.html_N9eUQPtWZCIy56EGyb9FJ3AtG/2HtxWy3lSD5KkWLXx4zl7Js0EXy03pqkfFKIfpT9uZE.php
@@ -0,0 +1,211 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/business_responsive_theme/templates/paragraph/paragraph--hero-area.html.twig */
+class __TwigTemplate_e1ae623564a0cb1b9a22143302549e84838027dcc9c3942d35af25d5452b74f7 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'paragraph' => [$this, 'block_paragraph'],
+            'content' => [$this, 'block_content'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 42, "block" => 49, "if" => 55];
+        $filters = ["clean_class" => 44, "escape" => 50];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'block', 'if'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 42
+        $context["classes"] = [0 => "paragraph", 1 => ("paragraph--type--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed($this->getAttribute(        // line 44
+($context["paragraph"] ?? null), "bundle", [])))), 2 => ((        // line 45
+($context["view_mode"] ?? null)) ? (("paragraph--view-mode--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null))))) : ("")), 3 => (( !$this->getAttribute(        // line 46
+($context["paragraph"] ?? null), "isPublished", [], "method")) ? ("paragraph--unpublished") : (""))];
+        // line 49
+        $this->displayBlock('paragraph', $context, $blocks);
+    }
+
+    public function block_paragraph($context, array $blocks = [])
+    {
+        // line 50
+        echo "  <div";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+        echo ">
+  
+    ";
+        // line 52
+        $this->displayBlock('content', $context, $blocks);
+        // line 68
+        echo "  </div>
+";
+    }
+
+    // line 52
+    public function block_content($context, array $blocks = [])
+    {
+        // line 53
+        echo "      ";
+        // line 54
+        echo "
+      ";
+        // line 55
+        if ($this->getAttribute(($context["content"] ?? null), "field_hero_bg_image", [])) {
+            // line 56
+            echo "       
+        <div class=\"container-fluid banner-slider\">
+         ";
+            // line 58
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["content"] ?? null), "field_hero_bg_image", [])), "html", null, true);
+            echo "
+         <div class=\"banner-floting\">
+         ";
+            // line 60
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["content"] ?? null), "field_title", [])), "html", null, true);
+            echo " ";
+            // line 61
+            echo "         </div>         
+        </div>             
+              
+      
+      ";
+        }
+        // line 65
+        echo "      
+      
+    ";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/business_responsive_theme/templates/paragraph/paragraph--hero-area.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  110 => 65,  103 => 61,  100 => 60,  95 => 58,  91 => 56,  89 => 55,  86 => 54,  84 => 53,  81 => 52,  76 => 68,  74 => 52,  68 => 50,  62 => 49,  60 => 46,  59 => 45,  58 => 44,  57 => 42,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Default theme implementation to display a paragraph.
+ *
+ * Available variables:
+ * - paragraph: Full paragraph entity.
+ *   Only method names starting with \"get\", \"has\", or \"is\" and a few common
+ *   methods such as \"id\", \"label\", and \"bundle\" are available. For example:
+ *   - paragraph.getCreatedTime() will return the paragraph creation timestamp.
+ *   - paragraph.id(): The paragraph ID.
+ *   - paragraph.bundle(): The type of the paragraph, for example, \"image\" or \"text\".
+ *   - paragraph.getOwnerId(): The user ID of the paragraph author.
+ *   See Drupal\\paragraphs\\Entity\\Paragraph for a full list of public properties
+ *   and methods for the paragraph object.
+ * - content: All paragraph items. Use {{ content }} to print them all,
+ *   or print a subset such as {{ content.field_example }}. Use
+ *   {{ content|without('field_example') }} to temporarily suppress the printing
+ *   of a given child element.
+ * - attributes: HTML attributes for the containing element.
+ *   The attributes.class element may contain one or more of the following
+ *   classes:
+ *   - paragraphs: The current template type (also known as a \"theming hook\").
+ *   - paragraphs--type-[type]: The current paragraphs type. For example, if the paragraph is an
+ *     \"Image\" it would result in \"paragraphs--type--image\". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - paragraphs--view-mode--[view_mode]: The View Mode of the paragraph; for example, a
+ *     preview would result in: \"paragraphs--view-mode--preview\", and
+ *     default: \"paragraphs--view-mode--default\".
+ * - view_mode: View mode; for example, \"preview\" or \"full\".
+ * - logged_in: Flag for authenticated user status. Will be true when the
+ *   current user is a logged-in member.
+ * - is_admin: Flag for admin user status. Will be true when the current user
+ *   is an administrator.
+ *
+ * @see template_preprocess_paragraph()
+ *
+ * @ingroup themeable
+ */
+#}
+{%
+  set classes = [
+    'paragraph',
+    'paragraph--type--' ~ paragraph.bundle|clean_class,
+    view_mode ? 'paragraph--view-mode--' ~ view_mode|clean_class,
+    not paragraph.isPublished() ? 'paragraph--unpublished'
+  ]
+%}
+{% block paragraph %}
+  <div{{ attributes.addClass(classes) }}>
+  
+    {% block content %}
+      {# content #}
+
+      {% if content.field_hero_bg_image%}
+       
+        <div class=\"container-fluid banner-slider\">
+         {{content.field_hero_bg_image}}
+         <div class=\"banner-floting\">
+         {{content.field_title}} {#{content.field_hero_title_link}#}
+         </div>         
+        </div>             
+              
+      
+      {% endif %}      
+      
+    {% endblock %}
+  </div>
+{% endblock paragraph %}
+", "themes/business_responsive_theme/templates/paragraph/paragraph--hero-area.html.twig", "C:\\xampp\\htdocs\\fly2houston\\themes\\business_responsive_theme\\templates\\paragraph\\paragraph--hero-area.html.twig");
+    }
+}
